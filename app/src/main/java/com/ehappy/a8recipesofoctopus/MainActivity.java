ee641b8cbf957bd6efad774c9e995b866e0193ee
Morris Tai <leaper151@outlook.com>
2018-04-11 22:41:44 +0800

    add first intent

--- app/src/main/java/com/ehappy/a8recipesofoctopus/MainActivity.java
+++ app/src/main/java/com/ehappy/a8recipesofoctopus/MainActivity.java
@@ -1,18 +1,25 @@
 package com.ehappy.a8recipesofoctopus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+//    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    /** Called when the user taps the Send button */
     public void openRecipe(View view) {
-        // Do something in response to button
+        Intent intent = new Intent(this, OcTo1.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/ehappy/a8recipesofoctopus/OcTo1.java
+++ app/src/main/java/com/ehappy/a8recipesofoctopus/OcTo1.java
@@ -1,7 +1,9 @@
 package com.ehappy.a8recipesofoctopus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OcTo1 extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class OcTo1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oc_to1);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+//        TextView textView = findViewById(R.id.textView);
+//        textView.setText(message);
     }
 }
