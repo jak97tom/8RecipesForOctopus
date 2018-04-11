dfe029beb39930bb9419a9ea3007dbfc8bf7e51b
Morris Tai <leaper151@outlook.com>
2018-04-11 21:32:06 +0800

    add OcTo1

--- app/src/main/java/com/ehappy/a8recipesofoctopus/MainActivity.java
+++ app/src/main/java/com/ehappy/a8recipesofoctopus/MainActivity.java
@@ -2,6 +2,7 @@ package com.ehappy.a8recipesofoctopus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openRecipe(View view) {
+        // Do something in response to button
     }
 }
---
+++ app/src/main/java/com/ehappy/a8recipesofoctopus/OcTo1.java
@@ -0,0 +1,13 @@
+package com.ehappy.a8recipesofoctopus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OcTo1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oc_to1);
+    }
+}
