665e73bd0c4ba9d6654867ef99bd2a070792d6cd
Morris Tai <leaper151@outlook.com>
2018-04-12 09:33:52 +0800

    add new actionbar

--- app/src/main/java/com/ehappy/a8recipesofoctopus/MainActivity.java
+++ app/src/main/java/com/ehappy/a8recipesofoctopus/MainActivity.java
@@ -3,8 +3,13 @@ package com.ehappy.a8recipesofoctopus;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+//import android.widget.EditText;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 //    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -12,7 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
 
     /** Called when the user taps the Send button */
     public void openRecipe(View view) {
