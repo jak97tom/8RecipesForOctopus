bb96a6c52e24122489d1dbdb2044195a8ba539ee
Morris Tai <leaper151@outlook.com>
2018-04-18 14:46:25 +0800

    added sub actionbar

--- app/src/main/java/com/ehappy/a8recipesofoctopus/OcTo1.java
+++ app/src/main/java/com/ehappy/a8recipesofoctopus/OcTo1.java
@@ -1,16 +1,24 @@
 package com.ehappy.a8recipesofoctopus;
 
 import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class OcTo1 extends AppCompatActivity {
 
+public class OcTo1 extends AppCompatActivity {
+//    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oc_to1);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+//        返回鍵
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
