d5f008064d1468d5755d23e6b453e2252fd3977f
Morris Tai <diaper151@gmail.com>
2018-06-18 21:23:11 +0800

    新增小常識內容

--- app/src/main/java/com/ehappy/a8recipesofoctopus/MainActivity.java
+++ app/src/main/java/com/ehappy/a8recipesofoctopus/MainActivity.java
@@ -11,8 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ShareActionProvider;
 import android.widget.Toast;
-
-import java.time.Instant;
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -50,8 +49,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(browserIntent);
                 break;
             case R.id.help:
-                Intent intent = new Intent(this, OcHint.class);
-                startActivity(intent);
+                String[] names={"小卷體內的透明幾丁質內殼較寬，各腕具有2列吸盤，但不會變形為鉤狀構造。",
+                        "烏賊俗稱花枝，在口部周圍具有10隻附肢",
+                        "魷魚有10隻附肢，其中8隻為腕，另2隻為具有伸縮性的觸腕",
+                        "透抽外套膜較為細長，身體後半段有一對長菱形的鰭",
+                        "章魚在口部周圍僅有8隻腕，缺乏觸腕，故又被稱作八爪魚"};
+                int i = (int) (Math.random() * names.length);
+                String element = names[i];
+                Toast.makeText(getApplicationContext(),element,Toast.LENGTH_SHORT).show();
                 break;
             case R.id.share:
                 final Intent fbIntent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.facebook.com/sharer/sharer.php?u=https%3A//play.google.com/store/apps/details?id=com.ehappy.a8recipesofoctopus"));
